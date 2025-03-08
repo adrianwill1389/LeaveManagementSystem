5a32eb5371b0f2e220645cdec35a32cde2b97f95
Adrian Williams <adrianwilliams1389@gmail.com>
2025-03-08 13:17:25 -0500

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new Models.TestViewModel
+            {
+                Name = "Student of MVC Mastery",
+                DateOfBirth = new DateTime(1964, 12, 01)
+            };
+            return View(data);
+        }
+    }
+}
